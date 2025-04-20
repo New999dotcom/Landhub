2ceb41888801b3bd94a56afb08329b6c00ed10f9
New999dotcom <pranavsunny999@gmail.com>
2025-04-20 12:32:38 +0530

    Update wsgi.py

--- primelandhub/primelandhub/wsgi.py
+++ primelandhub/primelandhub/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'primelandhub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'primelandhub.primelandhub.settings')
 
 application = get_wsgi_application()
