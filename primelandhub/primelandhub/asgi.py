ea3d171695f0304852f6778596d891515bf24829
New999dotcom <pranavsunny999@gmail.com>
2025-04-20 13:36:40 +0530

    Update asgi.py

--- primelandhub/primelandhub/asgi.py
+++ primelandhub/primelandhub/asgi.py
@@ -1,14 +1,16 @@
-# your_project_name/asgi.py
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-import landhub.routing
+from channels.auth import AuthMiddlewareStack  # Add this for WebSocket auth
+import landhub.routing  # Make sure this exists
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'primelandhub.settings')  # Changed from your_project_name
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': URLRouter(
-        landhub.routing.websocket_urlpatterns
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(  # Added AuthMiddleware
+        URLRouter(
+            landhub.routing.websocket_urlpatterns
+        )
     ),
 })
